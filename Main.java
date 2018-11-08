b5269ff1332373506dc9d3e7baff44cd87753f10
trembleyluke <42677977+trembleyluke@users.noreply.github.com>
2018-11-08 07:51:48 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,58 +1,29 @@
 public class Main {
-    public static double workTime, devs, sprintLength, capacity, time;
+    public static double workTime, capacity, time;
+
 
     public static void main(String[] args) {
         workTime = 1590;
-        devs= 8;
-        sprintLength= 3;
-        hours30();
-        hours35();
-        hours40();
-        devs= 9;
-        sprintLength= 3;
-        hours30();
-        hours35();
-        hours40();
-        devs= 10;
-        sprintLength= 3;
-        hours30();
-        hours35();
-        hours40();
-        devs= 11;
-        sprintLength= 3;
-        hours30();
-        hours35();
-        hours40();
-        devs= 8;
-        sprintLength= 4;
-        hours30();
-        hours35();
-        hours40();
-        devs= 8;
-        sprintLength= 5;
-        hours30();
-        hours35();
-        hours40();
-        devs= 8;
-        sprintLength= 6;
-        hours30();
-        hours35();
-        hours40();
+        printCapacity(8,3);
+        printCapacity(9,3);
+        printCapacity(10,3);
+        printCapacity(11,3);
+        printCapacity(8,4);
+        printCapacity(8,5);
+        printCapacity(8,6);
     }
-    public static void hours30(){
-        time = devs * 30 * sprintLength;
+    public static void printCapacity(int devs, int sprintLength){
+        time = devs * 30.0 * sprintLength;
         capacity = (workTime/time * 100);
-        System.out.printf("With %.0f devs working a %.0f week sprint, for 30 hrs/wk, we would be at %.0f%% of capacity.\n", devs,sprintLength, capacity);
-    }
-    public static void hours35(){
-        time = devs * 35 * sprintLength;
+        System.out.printf("With %d devs working a %d week sprint, for 30 hrs/wk, we would be at %.0f%% of capacity.\n", devs,sprintLength, capacity);
+
+        time = devs * 35.0 * sprintLength;
         capacity = (workTime/time * 100);
-        System.out.printf("With %.0f devs working a %.0f week sprint, for 35 hrs/wk, we would be at %.0f%% of capacity.\n", devs,sprintLength, capacity);
-    }
-    public static void hours40(){
-        time = devs * 40 * sprintLength;
+        System.out.printf("With %d devs working a %d week sprint, for 35 hrs/wk, we would be at %.0f%% of capacity.\n", devs,sprintLength, capacity);
+
+        time = devs * 40.0 * sprintLength;
         capacity = (workTime/time * 100);
-        System.out.printf("With %.0f devs working a %.0f week sprint, for 40 hrs/wk, we would be at %.0f%% of capacity.\n", devs,sprintLength, capacity);
+        System.out.printf("With %d devs working a %d week sprint, for 40 hrs/wk, we would be at %.0f%% of capacity.\n", devs, sprintLength, capacity);
         System.out.println();
     }
 }
